c6cceefbaf76e72f0132d847dc456d2f4111e945
zhangsjie <852618587@qq.com>
2023-10-23 17:54:27 +0800

    icbc

--- internal/sdk/stru/icbc_bank.go
+++ internal/sdk/stru/icbc_bank.go
@@ -1,6 +1,132 @@
 package stru
 
-//通用接口
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"gitlab.yoyiit.com/youyi/app-bank/internal/enum"
+	"gitlab.yoyiit.com/youyi/go-core/config"
+	"gitlab.yoyiit.com/youyi/go-core/util"
+	"go.uber.org/zap"
+	"net/url"
+	"sort"
+	"strings"
+	"time"
+)
+
+func NewIcbcGlobalRequest() *IcbcGlobalRequest {
+	msgId, _ := util.SonyflakeID()
+	request := IcbcGlobalRequest{
+		AppID:     config.GetString(enum.IcbcAppId, ""),
+		MsgID:     msgId,
+		SignType:  "RSA2",
+		Timestamp: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	return &request
+}
+
+func GenerateParams(request IcbcGlobalRequest) (*IcbcGlobalRequest, error) {
+	// Step 1: 参数排序
+	params := make(map[string]string)
+	params["app_id"] = request.AppID
+	params["msg_id"] = request.MsgID
+	params["sign_type"] = request.SignType
+	params["timestamp"] = request.Timestamp
+
+	bizContent, err := MarshalBizContent(request.BizContent)
+	if err != nil {
+		fmt.Println("Error marshaling biz_content:", err)
+		return nil, err
+	}
+	params["biz_content"] = bizContent
+
+	sortedParamKeys := sortParams(params)
+	// Step 2: 构造签名文本
+	signaturePlain := constructSignaturePlain(sortedParamKeys, params)
+
+	// Step 3: Sign the signature plain text with RSA private key
+	privateKey := []byte("your_private_key")
+	signature, err := signWithRSA(signaturePlain, privateKey)
+	if err != nil {
+		fmt.Println("Error signing with RSA:", err)
+		return nil, err
+	}
+
+	request.Sign = signature
+	zap.L().Info(fmt.Sprintf("ICBCGenerateParams==%+v\n", request))
+	return &request, nil
+}
+
+func sortParams(params map[string]string) []string {
+	keys := make([]string, 0, len(params))
+	for key := range params {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func constructSignaturePlain(keys []string, params map[string]string) string {
+	var signatureStrings []string
+	for _, key := range keys {
+		value := params[key]
+		encodedValue := url.QueryEscape(value)
+		signatureStrings = append(signatureStrings, fmt.Sprintf("%s=%s", key, encodedValue))
+	}
+	return strings.Join(signatureStrings, "&")
+}
+
+func signWithRSA(plainText string, privateKeyBytes []byte) (string, error) {
+	hashed := sha256.Sum256([]byte(plainText))
+
+	privateKey, err := parsePrivateKey(privateKeyBytes)
+	if err != nil {
+		return "", err
+	}
+
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed[:])
+	if err != nil {
+		return "", err
+	}
+
+	encodedSignature := base64.StdEncoding.EncodeToString(signature)
+	return encodedSignature, nil
+}
+
+func parsePrivateKey(privateKeyBytes []byte) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode(privateKeyBytes)
+	if block == nil {
+		return nil, fmt.Errorf("failed to parse PEM block containing the key")
+	}
+
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	rsaPrivateKey, ok := privateKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("private key is not an RSA private key")
+	}
+
+	return rsaPrivateKey, nil
+}
+
+func MarshalBizContent(bizContent interface{}) (string, error) {
+	jsonData, err := json.Marshal(bizContent)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonData), nil
+}
+
+// IcbcGlobalRequest 通用接口
 type IcbcGlobalRequest struct {
 	AppID      string      `json:"app_id"`      // APP的编号,应用在API开放平台注册时生成
 	MsgID      string      `json:"msg_id"`      // 消息通讯唯一编号，每次调用独立生成，APP级唯一
@@ -10,13 +136,7 @@ type IcbcGlobalRequest struct {
 	BizContent interface{} `json:"biz_content"` // 请求参数的集合
 }
 
-//通用接口
-type IcbcGlobalResponse struct {
-	Sign               string `json:"sign"`                 // 针对返回参数集合的签名
-	ResponseBizContent string `json:"response_biz_content"` // 响应参数集合，包含公共和业务参数
-}
-
-//签约参数
+// IcbcSignRequest 签约参数
 type IcbcSignRequest struct {
 	AppID      string        `json:"app_id"`               // APP的编号,应用在API开放平台注册时生成
 	ApiName    string        `json:"api_name"`             // api接口名称
@@ -51,7 +171,7 @@ type AccListItem struct {
 	StatementFlag string `json:"statementFlag"` // 开通账务明细查询标示，0：关；1：开
 }
 
-//单账号交易明细查询参数
+// AccDetailRequest 单账号交易明细查询参数
 type AccDetailRequest struct {
 	Fseqno    string `json:"fseqno"`    //合作方上送，需保证全局唯一，每次调用校验表里是否重复；建议拼接携带一级合作方编号、调用类型（0单账户流水提取；1流水批量提取；2回单下载）；
 	Account   string `json:"account"`   //银行卡号
